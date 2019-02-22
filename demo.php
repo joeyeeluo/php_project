3977205858d578192d25c53fde286150cadeaf0a
luozhuoyu <1124039238@qq.com>
2019-02-22 14:49:50 +0800

    我的第一个版本

---
+++ demo.php
@@ -0,0 +1,6 @@
+<?php
+require_once './vendor/autoload.php';
+use Project\Start\Tools;
+ $tools = new Tools;
+(new Tools)->hello();
+//Tools::hello();
---
+++ src/Project/Start/Tools.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Create by PhpStorm.
+ * User:luozhuoyu
+ * Date:2019/02/22 14:16
+ */
+
+namespace Project\start;
+
+class Tools{
+	public function __construct()
+	{
+
+	}
+
+	public function hello() 
+	{
+		echo "hello world!!!";
+	}
+}
